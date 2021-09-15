2f9b1fb004e8b5c5418f2215973a93ccb0be72c6
Pav_lov_sky <infopavlovskiy@gmail.com>
2021-09-15 12:51:35 +0300

    finish

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -3,26 +3,24 @@ const users = [
     { name: 'Poly', active: false },
     { name: 'Ajax', active: true },
     { name: 'Lux', active: false },
-  ];
+  ]
   
-  const toggleUserState = (allUsers, userName, callback) => {
-    const updatedUsers = allUsers.map(user =>
-      user.name === userName ? { ...user, active: !user.active } : user,
-    );
+  const toggleUserState = (allUsers, userName) => {
+    return Promise.resolve(allUsers.map(user =>
+      (user.name === userName ? { ...user, active: !user.active } : user
+    )))
+  }
   
-    callback(updatedUsers);
-  };
-  
-  const logger = updatedUsers => console.table(updatedUsers);
+  const logger = result => console.table(result)
   
   /*
    * Сейчас работает так
    */
-  toggleUserState(users, 'Mango', logger);
-  toggleUserState(users, 'Lux', logger);
+  // toggleUserState(users, 'Mango', logger);
+  // toggleUserState(users, 'Lux', logger);
   
   /*
    * Должно работать так
    */
-  toggleUserState(users, 'Mango').then(logger);
-  toggleUserState(users, 'Lux').then(logger);
+  toggleUserState(users, 'Mango').then(logger)
+  toggleUserState(users, 'Lux').then(logger)
